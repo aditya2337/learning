98b2615ac9a80da196eca29a1ba242e765617d62
aditya <androiditya@gmail.com>
2020-08-29 10:48:28 +0530

    some optimisation stuff

--- optimisation/index.js
+++ optimisation/index.js
@@ -1,18 +1,35 @@
 const { performance , PerformanceObserver} = require('perf_hooks');
 
+performance.mark('start')
+
+class Point {
+  constructor(x, y, z) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+}
+
+var iterations = 100000
+
+while (iterations--) {
+  const point = new Point(2, 4, 5)
+  delete point.z
+
+  JSON.stringify(point)
+}
+// function add(a, b) {
+//   return a + b
+// }
+
+performance.mark('end')
+
 const obs = new PerformanceObserver((list, observer) => {
   console.log(list.getEntriesByName('My Special Benchmark'))
 })
 
 obs.observe({ entryTypes: ['measure']  })
 
-performance.mark('start')
-
-function add(a, b) {
-  return a + b
-}
-
-performance.mark('end')
 
 performance.measure('My Special Benchmark', 'start', 'end');
 
