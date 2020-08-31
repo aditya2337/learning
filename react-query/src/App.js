6920f6bbec0cfeaa42bbbab6fec4de5bc531a33f
aditya <androiditya@gmail.com>
2020-08-31 10:39:11 +0530

    Learning react query now 🙆🏽‍♂️

---
+++ react-query/src/App.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import { ReactQueryDevtools } from 'react-query-devtools'
+import './App.css';
+
+function useApi(api) {
+  return useQuery(
+    api,
+    async () => {
+      return axios.get(`https://pokeapi.co/api/v2/${api}`).then(res => res.data.results)
+    },
+    {
+      // refetchOnWindowFocus: false
+      // staleTime: 5000
+      cacheTime: 5000
+    }
+  )
+}
+
+function usePokemonSeach(pokemon) {
+  return useQuery(
+    pokemon,
+    async () => axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then(res => res.data),
+    {
+      enabled: pokemon
+    }
+  )
+}
+
+function Count() {
+  const queryInfo = useApi('pokemon')
+  return <h3>You are looking at {queryInfo.data?.length} pokemons</h3>
+}
+
+function Berries() {
+  const queryInfo = useApi('berry')
+  return queryInfo.isLoading ? 'Loading...' : queryInfo.isError ? queryInfo.error.message : (
+    <div className="App">
+      {queryInfo.data.map(result => <div key={result.name}>{result.name}</div>)}
+      <br />
+      {queryInfo.isFetching ? 'Updating...' : null}
+    </div>
+  );
+}
+
+function Pokemon() {
+  const queryInfo = useApi('pokemon')
+  return queryInfo.isLoading ? 'Loading...' : queryInfo.isError ? queryInfo.error.message : (
+    <div className="App">
+      {queryInfo.data.map(result => <div key={result.name}>{result.name}</div>)}
+      <br />
+      {queryInfo.isFetching ? 'Updating...' : null}
+    </div>
+  );
+}
+
+function PokemonSearch({ pokemon  }) {
+  const queryInfo = usePokemonSeach(pokemon)
+
+  return queryInfo.isLoading ? "Loading..." : queryInfo.isError ? queryInfo.error.message : 
+    queryInfo?.data?.sprites ? (
+      <img src={queryInfo.data.sprites.front_default} />
+    ) : null
+
+}
+
+function App() {
+  const [show, toggle] = React.useState(true)
+  const [pokemon, setPokemon] = React.useState('')
+
+  return (
+    <>
+      <input value={pokemon} onChange={e => setPokemon(e.target.value)} />
+      <button onClick={() => toggle(!show)}>Show</button>
+      <PokemonSearch pokemon={pokemon} />
+      {show ? <Pokemon /> : null}
+      <ReactQueryDevtools />
+      <hr />
+      {false && <Berries />}
+    </>
+  )
+}
+
+export default App;
